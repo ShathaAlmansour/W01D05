c652aa2abd061048cef5022b66510401311995c0
ShathaAlmansour <>
2021-10-14 13:10:42 +0300

    added file

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+let myname= "Shatha"
+let age=24
+let main= [1,2,3]
+let car={name:"kia",modle:2020,pres:1500}
+console.log(typeof (myname))
+console.log(typeof (age))
+console.log(typeof (main))
+console.log(typeof (car))
+
+let sh= ""
+console.log("")
+
+console.log("")
+
---
+++ myVariables.js
@@ -0,0 +1,18 @@
+let language; 
+console.log("JavaScript")
+
+let length;
+length = 10;
+
+let width;
+width = 10;
+
+console.log (length*width);
+
+let num1;
+num1 = 5;
+
+let num2;
+num2 = 2;
+
+console.log(num1%num2);
